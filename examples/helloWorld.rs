0fc440b0431bcc236ff1838c2f0109d6cfbfb575
Kees van Beilen <k.beilen@icloud.com>
2022-08-20 21:05:31 +0200

    modified the example

--- examples/helloWorld.rs
+++ examples/helloWorld.rs
@@ -3,28 +3,24 @@ use gappkit::prelude::*;
 
 fn main(){
     App::new("hello world")
-    .on_launch(launch)
-    .run()
+        .on_launch(launch)
+        .run()
 }
-fn launch(app:&App){
-    println!("application did finish launching");
-    // Window::new(())
-    // println!("{}",app.config.name);
-
+fn launch(_app:&App){
     Window::new(WindowDescriptor{
         title:"title".to_string()
     }).ui(
         View{
             children:vec![
-                Box::new(ImageView::new("/Users/kbeilen/Desktop/homework/coding/cargoProjects/gappkit/resources/paris.jpeg"))
+                Box::new(
+                    HStackView::new(vec![
+                        Box::new(ImageView::new(format!("{}/resources/paris.jpeg",std::env::current_dir().unwrap().to_str().unwrap_or("./")).as_str())),
+                        Box::new(ImageView::new(format!("{}/resources/london.jpeg",std::env::current_dir().unwrap().to_str().unwrap_or("./")).as_str())),
+                        Box::new(TextView::new("Hello world".to_string()))
+                    ])
+                )
             ],
             ..Default::default()
         }
     );
-        // .ui(View!{
-        //     HStack!{
-        //         Text!("Hello world")
-        //         Text!("Hello world")
-        //     }
-        // })
 }
