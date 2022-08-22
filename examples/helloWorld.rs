6450a6726cc6ad4e2b6648392652748c61a50c79
Kees van Beilen <k.beilen@icloud.com>
2022-08-22 16:58:16 +0200

    added macro ui system

--- examples/helloWorld.rs
+++ examples/helloWorld.rs
@@ -6,21 +6,51 @@ fn main(){
         .on_launch(launch)
         .run()
 }
-fn launch(_app:&App){
-    Window::new(WindowDescriptor{
-        title:"title".to_string()
-    }).ui(
-        View{
-            children:vec![
-                Box::new(
-                    HStackView::new(vec![
-                        Box::new(ImageView::new(format!("{}/resources/paris.jpeg",std::env::current_dir().unwrap().to_str().unwrap_or("./")).as_str())),
-                        Box::new(ImageView::new(format!("{}/resources/london.jpeg",std::env::current_dir().unwrap().to_str().unwrap_or("./")).as_str())),
-                        Box::new(TextView::new("Hello world".to_string()))
-                    ])
-                )
-            ],
-            ..Default::default()
-        }
-    );
+
+// fn click(btn:&ButtonView){
+//     println!("clicked")
+// }
+fn launch(app:&mut App){
+
+    Window::new("test window")
+        .ui(View!{
+            VStack!{
+                Image!(format!("{}/resources/paris.jpeg",std::env::current_dir().unwrap().to_str().unwrap_or("./")).as_str())
+                    .scale_aspect()
+                Text!("This is a public domain image".to_string())
+            }
+        });
+    // let mut wind = Window::new(WindowDescriptor{
+    //     title:"title".to_string()
+    // });
+    // let mut root = View{
+    //     ..Default::default()
+    // };
+    // root.children.push({
+    //     let mut btn = ButtonView::new("Hello world".to_string());
+    //     btn.on_click(click);
+    //     Box::new(btn)
+    // });
+
+    // let mut btn = ButtonView::new("Hello world".to_string());
+    // btn.on_click(click);
+    // wind.ui(
+    //     View{
+    //         children:vec![
+    //             Box::new(
+    //                 VStackView::new(vec![
+    //                     Box::new(ImageView::new(format!("{}/resources/paris.jpeg",std::env::current_dir().unwrap().to_str().unwrap_or("./")).as_str())),
+    //                     Box::new(
+    //                         btn
+    //                     ),
+    //                     Box::new(ImageView::new(format!("{}/resources/london.jpeg",std::env::current_dir().unwrap().to_str().unwrap_or("./")).as_str())),
+    //                 ])
+    //             )
+    //         ],
+    //         ..Default::default()
+    //     }
+    // );
+    // //keeps the window structs alive
+    // app.windows.push(wind);
+    
 }
---
+++ src/ui_macros/mod.rs
@@ -0,0 +1,70 @@
+use crate::ui::{View};
+#[macro_export]
+macro_rules! View {
+    ( $( $x:expr )* ) => {
+        {
+            let mut view = gappkit::ui::View{
+                ..Default::default()
+            };
+            $(
+                view.children.push($x);
+            )*
+            view
+        }
+    };
+}
+#[macro_export]
+macro_rules! Text {
+    ( $x:expr ) => {
+        {
+            let mut text_view = gappkit::ui::TextView::new($x);
+            Box::new(text_view)
+        }
+    };
+}
+#[macro_export]
+macro_rules! HStack {
+    ( $( $x:expr )* ) => {
+        {
+            let mut view = gappkit::ui::HStackView{
+                children:vec![$(
+                    ($x),
+                )*]
+            };
+            
+            Box::new(view)
+        }
+    };
+}
+#[macro_export]
+macro_rules! VStack {
+    ( $( $x:expr )* ) => {
+        {
+            let mut view = gappkit::ui::VStackView{
+                children:vec![$(
+                    ($x),
+                )*]
+            };
+            Box::new(view)
+        }
+    };
+}
+#[macro_export]
+macro_rules! Image {
+    ( $x:expr) => {
+        {
+            let mut image_view = gappkit::ui::ImageView::new($x);
+            Box::new(image_view)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! _ScaleAspect {
+    ( $x:expr ) => {
+        {
+            let mut image_view = gappkit::ui::ImageView::new($x);
+            Box::new(image_view)
+        }
+    };
+}
